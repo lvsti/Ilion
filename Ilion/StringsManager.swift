ee8838ec7677c54ca369b7d565dd483feeb03866
Tamas Lustyik <elveestei@gmail.com>
2017-04-18 22:30:20 +0200

    Added localized strings manager

---
+++ Ilion/StringsManager.swift
@@ -0,0 +1,127 @@
+//
+//  StringsManager.swift
+//  Visual10n
+//
+//  Created by Tamas Lustyik on 2017. 03. 15..
+//  Copyright © 2017. Tamas Lustyik. All rights reserved.
+//
+
+import Foundation
+
+func localizedString(_ key: String, comment: String = "") -> String {
+    return StringsManager.defaultManager.localizedString(key, comment: comment)
+}
+
+struct StringsEntry {
+    var locKey: String
+    var sourceText: String
+    var translatedText: String
+    var overrideText: String?
+    var resourceName: String
+}
+
+class StringsManager {
+
+    let storedOverridesKey = "Ilion.TranslationOverrides"
+
+    private let userDefaults: UserDefaults
+    
+    private var stringsFiles: [URL: [String: String]]
+    private let locRegex: NSRegularExpression
+    private var overriddenKeys: Set<String>
+    
+    private(set) var strings: [String: StringsEntry]
+    
+    static let defaultManager = StringsManager(userDefaults: UserDefaults.standard)
+    
+    private init(userDefaults: UserDefaults) {
+        self.userDefaults = userDefaults
+        
+        stringsFiles = [:]
+        overriddenKeys = []
+        strings = [:]
+        locRegex = try! NSRegularExpression(pattern: "^\\s*\"([^\"]+)\"\\s*=\\s*\"(.*)\"\\s*;\\s*$", options: [])
+
+        loadStringsFilesInBundle(Bundle.main)
+        
+        let storedOverrides: [String: String] = userDefaults.dictionary(forKey: storedOverridesKey) as? [String: String] ?? [:]
+        overriddenKeys = Set<String>(storedOverrides.keys)
+        for override in storedOverrides {
+            strings[override.key]?.overrideText = override.value
+        }
+    }
+    
+    private func loadStringsFilesInBundle(_ bundle: Bundle) {
+        let urls = bundle.urls(forResourcesWithExtension: "strings", subdirectory: nil, localization: "es")!
+        
+        for url in urls {
+            let translations = readLocalizedStringsFile(atPath: url.path)
+            for (sKey, sValue) in translations {
+                let entry = StringsEntry(locKey: sKey,
+                                         sourceText: "",
+                                         translatedText: sValue,
+                                         overrideText: nil,
+                                         resourceName: url.lastPathComponent)
+                strings[sKey] = entry
+            }
+            stringsFiles[url] = translations
+        }
+    }
+
+    private func readLocalizedStringsFile(atPath path: String) -> [String: String] {
+        let stringsFile = try! NSString(contentsOfFile: path, encoding: String.Encoding.utf8.rawValue)
+        var translations: [String: String] = [:]
+        
+        stringsFile.enumerateLines { line, stop in
+            let nsLine = line as NSString
+            if let match = self.locRegex.firstMatch(in: line, options: [], range: NSMakeRange(0, nsLine.length)) {
+                let key = nsLine.substring(with: match.rangeAt(1))
+                let value = nsLine.substring(with: match.rangeAt(2))
+                translations[key] = value
+            }
+        }
+        
+        return translations
+    }
+    
+    func addOverride(_ string: String, for key: String) {
+        overriddenKeys.insert(key)
+        strings[key]?.overrideText = string
+        
+        var storedOverrides = userDefaults.dictionary(forKey: storedOverridesKey) ?? [:]
+        storedOverrides[key] = string
+        userDefaults.setValue(storedOverrides, forKey: storedOverridesKey)
+    }
+    
+    func removeOverride(for key: String) {
+        overriddenKeys.remove(key)
+        strings[key]?.overrideText = nil
+
+        var storedOverrides = userDefaults.dictionary(forKey: storedOverridesKey) ?? [:]
+        storedOverrides[key] = nil
+        userDefaults.setValue(storedOverrides, forKey: storedOverridesKey)
+    }
+    
+    func removeAllOverrides() {
+        for key in overriddenKeys {
+            strings[key]?.overrideText = nil
+        }
+        overriddenKeys.removeAll()
+        
+        userDefaults.setValue([:], forKey: storedOverridesKey)
+    }
+    
+    func hasOverride(for key: String) -> Bool {
+        return strings[key]?.overrideText != nil
+    }
+
+    func localizedString(_ key: String, comment: String = "") -> String {
+        guard let entry = strings[key] else {
+            return key
+        }
+        
+        return entry.overrideText ?? entry.translatedText
+    }
+    
+}
+
