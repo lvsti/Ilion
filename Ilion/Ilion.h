b229cc6b3c0cd953d11bd9e6c4eb239fd101c228
Tamas Lustyik <elveestei@gmail.com>
2017-04-18 22:18:30 +0200

    Initial Commit

---
+++ Ilion/Ilion.h
@@ -0,0 +1,19 @@
+//
+//  Ilion.h
+//  Ilion
+//
+//  Created by Tamas Lustyik on 2017. 04. 18..
+//  Copyright © 2017. Tamas Lustyik. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Ilion.
+FOUNDATION_EXPORT double IlionVersionNumber;
+
+//! Project version string for Ilion.
+FOUNDATION_EXPORT const unsigned char IlionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ilion/PublicHeader.h>
+
+
