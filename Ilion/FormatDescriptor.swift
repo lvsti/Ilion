0022a0be6b74e966d4b4736b18c4e3730a7aa9eb
Tamas Lustyik <elveestei@gmail.com>
2017-06-17 22:22:54 +0200

    Extended format string parser to capture whole variable ranges

--- Ilion/FormatDescriptor.swift
+++ Ilion/FormatDescriptor.swift
@@ -26,28 +26,20 @@ struct FormatDescriptor {
         var placeholderTypes: [Int: String] = [:]
         
         try matches.forEach { match in
-            let range: NSRange
-            if match.rangeAt(3).location != NSNotFound {
-                // [dioux] are in range #3 because in #2 there may be length modifiers (like in "lld")
-                range = match.rangeAt(3)
-            } else {
-                // otherwise, no length modifier, the conversion specifier is in #2
-                range = match.rangeAt(2)
-            }
-            let char = (format as NSString).substring(with: range)
+            let typeRange = match.rangeAt(3)
+            let type = (format as NSString).substring(with: typeRange)
             
-            let posRange = match.rangeAt(1)
+            let posRange = match.rangeAt(2)
             let pair: (String, Int)
             
             if posRange.location == NSNotFound {
                 // No positional specifier
                 unusedIndex += 1
-                pair = (char, unusedIndex)
+                pair = (type, unusedIndex)
             } else {
-                // Remove the "$" at the end of the positional specifier, and convert to Int
-                let posRange1 = NSRange(location: posRange.location, length: posRange.length-1)
-                let pos = (format as NSString).substring(with: posRange1)
-                pair = (char, Int(pos)!)
+                // Convert the positional specifier to Int
+                let pos = (format as NSString).substring(with: posRange)
+                pair = (type, Int(pos)!)
             }
             
             if let type = placeholderTypes[pair.1] {
@@ -100,19 +92,26 @@ struct FormatDescriptor {
         return FormatDescriptor(placeholders: mergedPlaceholders)
     }
     
+    static func variableRanges(in format: String) -> [NSRange] {
+        let range = NSRange(location: 0, length: (format as NSString).length)
+        let matches = FormatDescriptor.formatTypesRegex.matches(in: format, options: [], range: range)
+        
+        return matches.map { $0.rangeAt(1) }
+    }
+    
     // extracted from: https://github.com/SwiftGen/SwiftGenKit/blob/master/Sources/Parsers/StringsFileParser.swift
     private static let formatTypesRegex: NSRegularExpression = {
         // %d/%i/%o/%u/%x with their optional length modifiers like in "%lld"
-        let pattern_int = "(?:h|hh|l|ll|q|z|t|j)?([dioux])"
+        let pattern_int = "(?:h|hh|l|ll|q|z|t|j)?[dioux]"
         // valid flags for float
         let pattern_float = "[aefg]"
         // like in "%3$" to make positional specifiers
-        let position = "([1-9]\\d*\\$)?"
+        let position = "(?:([1-9]\\d*)\\$)?"
         // precision like in "%1.2f"
-        let precision = "[-+]?\\d?(?:\\.\\d)?"
+        let precision = "[-+ 0#]*\\d?(?:\\.(?:\\d|\\*))?"
         
         return try! NSRegularExpression(
-            pattern: "(?:^|[^%]|(?<!%)(?:%%)+)%\(position)\(precision)(@|\(pattern_int)|\(pattern_float)|[csp])",
+            pattern: "(?:^|[^%]|(?<!%)(?:%%)+)(%\(position)\(precision)(@|\(pattern_int)|\(pattern_float)|[csp]))",
             options: [.caseInsensitive]
         )
     }()
