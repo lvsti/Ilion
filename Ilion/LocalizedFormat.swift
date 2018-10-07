37c77b1cd0987f7445af89fcc6bd054fcfc002b9
Tamas Lustyik <elveestei@gmail.com>
2018-10-07 22:30:24 +0200

    Fixed sorting of plural rules

--- Ilion/LocalizedFormat.swift
+++ Ilion/LocalizedFormat.swift
@@ -18,10 +18,8 @@ enum LocalizedFormatParseError: Error {
 }
 
 
-enum PluralRule: String {
+enum PluralRule: String, CaseIterable {
     case zero, one, two, few, many, other
-    
-    static let allValues: [PluralRule] = [.zero, .one, .two, .few, .many, .other]
 }
 
 extension PluralRule: Comparable {
@@ -73,7 +71,7 @@ struct LocalizedFormat {
                     throw LocalizedFormatParseError.missingOtherRule(varName: varName)
                 }
                 
-                let rulePairs = PluralRule.allValues
+                let rulePairs = PluralRule.allCases
                     .map { ($0, rules[$0.rawValue] ?? "") }
                     .filter { !$0.1.isEmpty }
                 
